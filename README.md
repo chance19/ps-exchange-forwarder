6b8d6aaae0345e191121f1ca8533b3625418b6bc
Chance <chance19@gmail.com>
2020-06-05 14:43:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ps-exchange-forwarder
+ 
---
+++ update.ps1
@@ -0,0 +1,12 @@
+﻿# Before running this script please update the import.csv file with relevant email addresses
+
+# Import data from csv
+$users = import-csv -path import.csv
+
+# For each entry in the csv create a new mail contact in active directory, in a specified OU and then set the exchange mailbox to deliver to that contact.
+foreach ($u in $users){
+
+    New-MailContact -Name $u.name -ExternalEmailAddress $u.fwd -OrganizationalUnit "OU=MailForwarders,DC=company,DC=com"
+    Set-Mailbox $u.eml -ForwardingAddress $u.fwd -DeliverToMailboxAndForward $false
+
+}
